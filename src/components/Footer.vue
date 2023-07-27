aa1429b9256c33fbb8c623d21b316bcd3a731e90
wezzganteng <fawwazalfaroz@gmail.com>
2023-07-27 11:52:56 +0700

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
+    <div class="footer">
     <h1>Footer</h1>
+</div>
   </template>
   
   <script>
@@ -9,7 +11,7 @@
   </script>
 
   <style>
-foooter {
+footer {
     width: 100%;
     margin: auto;
     display: flex;
