ec8e1f06f794258824d8c3d029ce1244790f251f
wezzganteng <fawwazalfaroz@gmail.com>
2023-07-27 12:02:59 +0700

    update

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,28 +1,28 @@
 
 <script setup>
-import { Carousel } from 'flowbite-vue'
-const pictures = [
-  {
-    'src': 'https://www.freepik.com/free-vector/hand-drawn-reseller-illustration_29822873.htm#query=reseler&position=1&from_view=search&track=sph',
-    'alt': 'Picture 1',
-  },
-  {
-    'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
-    'alt': 'Picture 2',
-  },
-  {
-    'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
-    'alt': 'Picture 3',
-  },
-  {
-    'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
-    'alt': 'Picture 4',
-  },
-  {
-    'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
-    'alt': 'Picture 5',
-  },
-]
+// import { Carousel } from 'flowbite-vue'
+// const pictures = [
+//   {
+//     'src': 'https://www.freepik.com/free-vector/hand-drawn-reseller-illustration_29822873.htm#query=reseler&position=1&from_view=search&track=sph',
+//     'alt': 'Picture 1',
+//   },
+//   {
+//     'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
+//     'alt': 'Picture 2',
+//   },
+//   {
+//     'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
+//     'alt': 'Picture 3',
+//   },
+//   {
+//     'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
+//     'alt': 'Picture 4',
+//   },
+//   {
+//     'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
+//     'alt': 'Picture 5',
+//   },
+// ]
 </script>
 <template>
   <div class="bk pt-20">
