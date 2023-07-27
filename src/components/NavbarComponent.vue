10f2c8a6e7c76ecd7c896df8ce3706a78c45354d
wezzganteng <fawwazalfaroz@gmail.com>
2023-07-27 11:47:13 +0700

    projek

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink } from 'flowbite-vue'
+import { Button } from 'flowbite-vue'
 </script>
 <template>
   <div class="nav">
@@ -18,7 +19,7 @@ import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink } from 'flowbite-vue'
         </NavbarCollapse>
       </template>
       <template #right-side>
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+        <Button gradient="purple-pink">Purple to pink</Button>
       </template>
     </Navbar></div>
 </template>
