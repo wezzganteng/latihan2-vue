edb01f6d0629b1c6ef1506712b281a40ea5715b7
wezzganteng <fawwazalfaroz@gmail.com>
2023-07-27 11:51:37 +0700

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,3 +7,18 @@
     name: 'Footer'
   }
   </script>
+
+  <style>
+foooter {
+    width: 100%;
+    margin: auto;
+    display: flex;
+    line-height: 80px;
+    position: sticky;
+    position: -webkit-sticky;
+    top: 0;
+    background: #3f72af;
+    z-index: 1;
+    border-top: solid #500ee9;
+}
+</style>
